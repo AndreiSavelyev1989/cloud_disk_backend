9ed70ca31e4140e4d2f7827634beffa6564ca1c6
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2023-12-29 18:39:10 +0300

    started server/connected mongodb/created user model

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Server");
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || config.get("serverPort");
+
+const start = async () => {
+  try {
+    mongoose
+      .connect(process.env.MONGODB_URI)
+      .then(() => {
+        console.log("Successful connection to MongoDB!");
+      })
+      .catch((err) => {
+        console.log("Error connecting to MongoDB:", err);
+      });
+    app.listen(PORT, () => {
+      console.log("Server started on port: " + PORT);
+    });
+  } catch (err) {}
+};
+
+start();
